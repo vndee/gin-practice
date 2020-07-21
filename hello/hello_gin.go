f045b08ed75d8784b973aac4b13c25625baf6524
vndee <hvd.huynhduy@gmail.com>
2020-07-21 15:33:10 +0700

    Initial commit

---
+++ hello/hello_gin.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"os"
+)
+
+
+func main() {
+	//gin.SetMode(gin.ReleaseMode)
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	r := gin.New()
+
+	r.GET("/", func(c * gin.Context) {
+		c.String(http.StatusOK, "Hello World")
+	})
+
+	r.GET("/ping", func(c * gin.Context) {
+		c.String(http.StatusOK, "pong")
+	})
+
+	log.Printf("Listening on port %s...", port)
+	r.Run(":" + port)
+}
